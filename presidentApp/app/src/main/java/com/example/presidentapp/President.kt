0775fe7d47828f07c80b5c2717782ab8132805af
LeeviPelkonen <Leevi.pelkonen@metropolia.fi>
2019-09-10 12:50:22 +0300

    lab 6 presidents

--- presidentApp/app/src/main/java/com/example/presidentapp/President.kt
+++ presidentApp/app/src/main/java/com/example/presidentapp/President.kt
@@ -1,4 +1,8 @@
 package com.example.presidentapp
 
-object President {
+class President(name:String, sYear:Int, eYear:Int, comment:String) {
+    val name = name
+    val startYear = sYear
+    val endYear = eYear
+    val comment = comment
 }
