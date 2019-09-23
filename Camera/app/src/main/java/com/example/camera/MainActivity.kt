269a39ad6c46f892cb9892893a6dcf562db5d1eb
LeeviPelkonen <32296789+LeeviPelkonen@users.noreply.github.com>
2019-09-23 12:28:07 +0300

    camera part2

--- Camera/app/src/main/java/com/example/camera/MainActivity.kt
+++ Camera/app/src/main/java/com/example/camera/MainActivity.kt
@@ -3,29 +3,45 @@ package com.example.camera
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    val REQUEST_IMAGE_CAPTURE = 99
+    val REQUEST_IMAGE_CAPTURE = 1
+    var mCurrentPhotoPath: String = ""
+    var imageFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener { takeImage() }
+        button.setOnClickListener { saveImage() }
     }
 
     fun saveImage(){
         val fileName = "temp_photo"
         val imgPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        var imageFile: File? = null
         imageFile = File.createTempFile(fileName, ".jpg", imgPath )
-
+        val photoURI: Uri = FileProvider.getUriForFile(
+            this,
+            "com.example.camera.fileprovider",
+            imageFile!!
+        )
+        mCurrentPhotoPath = imageFile!!.absolutePath
+        val myIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (myIntent.resolveActivity(packageManager) != null) {
+            myIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            startActivityForResult(myIntent, REQUEST_IMAGE_CAPTURE)
+        }
     }
 
     fun takeImage(){
@@ -38,8 +54,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, recIntent: Intent?) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            val extras = recIntent!!.extras
-            val imageBitmap = extras!!.get("data") as Bitmap
+            val imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath)
             imageView.setImageBitmap(imageBitmap)
         }
     }
