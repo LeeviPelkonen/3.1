7bdd1b70fb23d80fcff138652b66d1e6f0b46bf9
LeeviPelkonen <32296789+LeeviPelkonen@users.noreply.github.com>
2019-09-23 12:11:59 +0300

    camera app part1

--- Camera/app/src/main/java/com/example/camera/MainActivity.kt
+++ Camera/app/src/main/java/com/example/camera/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.camera
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 99
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener { takeImage() }
+    }
+
+    fun saveImage(){
+        val fileName = "temp_photo"
+        val imgPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        var imageFile: File? = null
+        imageFile = File.createTempFile(fileName, ".jpg", imgPath )
+
+    }
+
+    fun takeImage(){
+        val myIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val activityInfo = intent.resolveActivityInfo(packageManager, intent.flags)
+        if (activityInfo.exported) {
+            startActivityForResult(myIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, recIntent: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val extras = recIntent!!.extras
+            val imageBitmap = extras!!.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
     }
 }
